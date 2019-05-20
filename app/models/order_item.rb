db18c43e6d797f05867bd1b86b18b9dc4c674600
lpile <logan.pile@gmail.com>
2019-05-20 17:57:52 -0600

    Add relationships to model

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,3 +1,6 @@
 class OrderItem < ApplicationRecord
   validates_presence_of :quantity, :price, :fulfilled
+
+  belongs_to :item
+  belongs_to :order
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
+
   validates :email, uniqueness: true, presence: true
   validates_presence_of :password, :role, :active, :address, :city, :state, :zip, require: true
+
+  has_many :orders
+  has_many :items
 end
