707cb7298d9684c7d199d9dd70de355b24422310
davisc0801 <davisc1014@gmail.com>
2019-05-21 16:58:32 -0600

    Add sad path test for unknown user id

--- spec/features/user/show_spec.rb
+++ spec/features/user/show_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe "As a registered user" do
     it "shows me all data except password" do
       visit user_path(@user)
 
-      within("#user_info") do
+      within("#user-#{@user.id}-info") do
         expect(page).to have_content("Name: #{@user.name}")
         expect(page).to have_content("E-Mail: #{@user.email}")
         expect(page).to have_content("Address: #{@user.address}")
@@ -31,9 +31,23 @@ RSpec.describe "As a registered user" do
     it "shows a link to edit profile data" do
       visit user_path(@user)
 
-      within("#user_info") do
+      within("#user-#{@user.id}-info") do
         expect(page).to have_link("Edit your information")
       end
+    end
+
+    it "shows a 404 page if invalid user_id is entered" do
+      visit user_path(@user[:id] + 50)
+
+      expect(current_path).to eq(user_path(@user[:id] + 50))
+      expect(page.status_code).to eq(404)
+      expect(page).to_not have_content("Name: #{@user.name}")
+      expect(page).to_not have_content("E-Mail: #{@user.email}")
+      expect(page).to_not have_content("Address: #{@user.address}")
+      expect(page).to_not have_content("City: #{@user.city}")
+      expect(page).to_not have_content("State: #{@user.state}")
+      expect(page).to_not have_content("Zip: #{@user.zip}")
+      expect(page).to have_content("The page you were looking for doesn't exist")
     end
   end
 end
