090517fad675c07b6067a0439341e797c156c55c
lpile <logan.pile@gmail.com>
2019-05-20 21:36:46 -0600

    Add link to specs

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -10,9 +10,59 @@ RSpec.describe 'Welcome Index Spec:', type: :feature do
           expect(page).to have_link("Home", href: root_path)
           expect(page).to have_link("Items For Sale", href: items_path)
           expect(page).to have_link("Merchants", href: merchants_path)
-          expect(page).to have_link("My Shopping Cart", href: cart_path)
+          expect(page).to have_link("My Shopping Cart", href: carts_path)
           expect(page).to have_link("Log In", href: login_path)
           expect(page).to have_link("User Registration", href: new_user_path)
+        end
+      end
+
+      describe "I see 6 links in the navigation bar" do
+        it "when I click on 'Home', I go to home page" do
+          visit root_path
+
+          click_link "Home"
+
+          expect(current_path).to eq(root_path)
+        end
+
+        it "when I click on 'Items For Sale', I go to item's index page" do
+          visit root_path
+
+          click_link "Items For Sale"
+
+          expect(current_path).to eq(items_path)
+        end
+
+        it "when I click on 'Merchants', I go to merchant's index page" do
+          visit root_path
+
+          click_link "Merchants"
+
+          expect(current_path).to eq(merchants_path)
+        end
+
+        it "when I click on 'My Shopping Cart', I go to my shopping cart's index page" do
+          visit root_path
+
+          click_link "My Shopping Cart"
+
+          expect(current_path).to eq(carts_path)
+        end
+
+        it "when I click on 'Log In', I go to a log in form" do
+          visit root_path
+
+          click_link "Log In"
+
+          expect(current_path).to eq(login_path)
+        end
+
+        it "when I click on 'User Registration', I go to a user registration form" do
+          visit root_path
+
+          click_link "User Registration"
+
+          expect(current_path).to eq(new_user_path)
         end
       end
     end
