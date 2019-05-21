67aa41c624813b43f60aa9db82bec6877ba41c7f
Logan Pile <logan.pile@gmail.com>
2019-05-20 22:37:57 -0600

    Update order_item.rb
    Cleanup spacing

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,6 +1,7 @@
 class OrderItem < ApplicationRecord
   validates_presence_of :quantity, :price, :fulfilled
-validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10000}
+  validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10000}
+  
   belongs_to :item
   belongs_to :order
 end
