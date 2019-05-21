1b5af55b119df97a1378b6f3835690d64d18bedc
lpile <logan.pile@gmail.com>
2019-05-21 17:12:19 -0600

    Add class method .all_active

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,7 +6,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_many :order_items
   has_many :orders, through: :order_items
-  
+
+  def self.all_active
+    where(active: true)
+  end
+
   def deactivate
     update_attribute(:active, false)
   end
