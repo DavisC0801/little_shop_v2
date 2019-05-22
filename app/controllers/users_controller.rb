c492d454f61dc44da5dac7fdfbd3c01f2f1633c7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-22 16:23:49 -0600

    adds edit and create methods to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,23 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by_id(params[:format])
+    @user = current_user
     render file: "/public/404", status: 404 unless @user
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    user = User.find(current_user.id)
+    user.update(user_params)
+    redirect_to profile_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :address, :city, :state, :zip)
   end
 end
