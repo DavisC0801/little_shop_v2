c5274477805f928867a7f9a02b783078b985cac0
davisc0801 <davisc1014@gmail.com>
2019-05-24 17:36:25 -0600

    Add user validation helper method calls

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def new
     if current_user != nil
       flash[:message] = "You are already logged in."
-      redirect_to profile_path if current_user.role == "user"
-      redirect_to dashboard_path if current_user.role == "merchant"
-      redirect_to root_path if current_user.role == "admin"
+      redirect_to profile_path if current_user.user?
+      redirect_to dashboard_path if current_user.merchant?
+      redirect_to root_path if current_user.admin?
     end
   end
 
