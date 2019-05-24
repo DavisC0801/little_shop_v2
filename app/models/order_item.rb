f0c7ca4c4d495aa51f44ffa4907fb3b642d868d3
lpile <logan.pile@gmail.com>
2019-05-24 16:17:58 -0600

    Delete validates_presence_of :fulfilled because validates_inclusion_of does this already for boolean

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,5 +1,5 @@
 class OrderItem < ApplicationRecord
-  validates_presence_of :quantity, :price, :fulfilled, require: true
+  validates_presence_of :quantity, :price, require: true
   validates :quantity, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10000}
   validates_inclusion_of :fulfilled, :in => [true, false]
 
