e4024d3dcc37b1694a337a8bb3b0338aa6114cc8
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-25 16:06:19 -0600

    adds total_quantity and grand_total helper methods

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,4 +6,15 @@ class Order < ApplicationRecord
   has_many :items, through: :order_items
 
   enum role: %w(pending packaged shipped cancelled)
+
+
+  def total_quantity
+    items.count
+  end
+
+  def grand_total
+    items.sum do |item|
+      item.price
+    end
+  end
 end
