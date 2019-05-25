1c89177f78ccf5c24af3ad92994aed29848689a7
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-25 11:35:42 -0600

    adds route for user order page

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,10 @@ Rails.application.routes.draw do
   get '/profile/edit', to: 'users#edit', as: :profile_edit
 
   patch '/profile/edit', to: 'users#update'
+
+  scope '/user', as: 'profile' do
+    resources :orders
+  end
+
+  # get '/profile/orders', to: '', as: :profile_orders
 end
