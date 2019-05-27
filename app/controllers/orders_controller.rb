5dec599b35f1a6bc91b7e936e1207c9007b46120
Martin Mercer <28820023+m-mrcr@users.noreply.github.com>
2019-05-27 13:38:11 -0600

    adds cancel method (in progress)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,4 +2,17 @@ class OrdersController < ApplicationController
   def index
     @user = current_user
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def cancel
+    @order = Order.find(params[:id])
+    @order.make_items_unfulfilled
+    @order.cancel_order
+    @order.return_fulfilled_items
+    redirect_to profile_order_path(@order)
+    flash[:message] = "Order #{@order.id} has been cancelled"
+  end
 end
