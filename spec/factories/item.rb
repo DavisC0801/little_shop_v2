95c3b11d1bc063c01cba91206564121ed24e4812
Martha Troubh <indexer@icloud.com>
2019-05-29 15:47:06 -0600

    update factory

--- spec/factories/item.rb
+++ spec/factories/item.rb
@@ -1,10 +1,10 @@
 FactoryBot.define do
   factory :item do
     sequence(:name)             { |n| "Item #{n}" }
-    sequence(:description)      { |n| "#{n} Wonderful! " }
-    sequence(:price)            { |n| n*2 }
-    sequence(:inventory)        { |n| n*3 }
-    image                       { 'string' }
+    sequence(:description)      { |n| "Description #{n}" }
+    sequence(:price)            { |n| n }
+    sequence(:inventory)        { |n| n }
+    sequence(:image)            { |n| "https://picsum.photos/100/300?image=#{n}" }
     active                      { true }
     created_at                  { Time.now }
     updated_at                  { Time.now }
