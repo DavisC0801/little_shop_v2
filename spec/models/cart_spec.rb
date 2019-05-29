459971573089a03af4c0f49d7322e9acd0f44012
Martha Troubh <indexer@icloud.com>
2019-05-29 17:51:03 -0600

    test specs

--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -13,13 +13,11 @@ RSpec.describe Cart do
     it "adds a item to its contents" do
       subject.add_item(1)
       subject.add_item(2)
-
       expect(subject.contents).to eq({'1' => 3, '2' => 4})
     end
 
-    it "adds a item that hasn't been added yet" do
+    it "add new item" do
       subject.add_item('3')
-
       expect(subject.contents).to eq({'1' => 2, '2' => 3, '3' => 1})
     end
   end
@@ -29,28 +27,24 @@ RSpec.describe Cart do
       item_1 = create(:item)
       item_2 = create(:item)
       cart_1 = Cart.new({item_1.id.to_s => 2, item_2.id.to_s => 3})
-
       expect(cart_1.subtotal(item_1)).to eq(2 * item_1.price)
       expect(cart_1.subtotal(item_2)).to eq(3 * item_2.price)
     end
   end
 
   describe "#total" do
-    it "calculates the total cost" do
+    it "calculates the total" do
       item_1 = create(:item)
       item_2 = create(:item)
       cart_1 = Cart.new({item_1.id.to_s => 2, item_2.id.to_s => 3})
-
       expect(cart_1.total).to eq(2 * item_1.price + 3 * item_2.price)
     end
   end
 
-
   describe "#remove" do
-    it "remove a single item from cart" do
+    it "remove an item from cart" do
       subject.remove_item(1)
       subject.remove_item(2)
-
       expect(subject.contents).to eq({'1' => 1, '2' => 2})
       subject.remove_item(1)
       expect(subject.contents).to eq({'2' => 2})
@@ -60,7 +54,6 @@ RSpec.describe Cart do
   describe "#remove_all" do
     it "removes the item from cart" do
       subject.remove_all_item(1)
-
       expect(subject.contents).to eq({'2' => 3})
     end
   end
