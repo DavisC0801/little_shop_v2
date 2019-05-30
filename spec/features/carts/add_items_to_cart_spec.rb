fc53bd611bc49b40df31477158acdc88d51cccb8
davisc0801 <davisc1014@gmail.com>
2019-05-30 09:24:13 -0600

    Final refactor to include cart branch

--- spec/features/carts/add_items_to_cart_spec.rb
+++ spec/features/carts/add_items_to_cart_spec.rb
@@ -1,73 +1,51 @@
-# frozen_string_literal: true
-
 require 'rails_helper'
 
-RSpec.describe 'When a user adds an item to their cart', type: :feature do
-  before :each do
+RSpec.describe "as a visitor" do
+  before(:each) do
     @user = create(:user)
-    @item1, @item2 = create_list(:item, 2, user: @user)
+    @item_1 = create(:item, user: @user)
+    @item_2 = create(:item, user: @user)
   end
 
-  it 'allows me to add a item to the cart and displays a message' do
-    visit item_path(@item2)
+  it "displays a message" do
+    visit item_path(@item_1)
+    click_button "Add Item"
 
-    click_on "Add Item"
-
+    expect(page).to have_content("You now have 1 item of #{@item_1.name} in your cart.")
     expect(current_path).to eq(items_path)
-    expect(page).to have_content("You now have 1 item of #{@item2.name} in your cart.")
   end
-end
 
-require 'rails_helper'
+  it "the message for multiple items" do
+    visit item_path(@item_1)
+    click_button "Add Item"
 
-RSpec.describe "adding items to their cart" do
-  context "as a visitor" do
-    before(:each) do
-      @user = create(:user)
-      @item_1 = create(:item, user: @user)
-      @item_2 = create(:item, user: @user)
-    end
+    visit item_path(@item_2)
+    click_button "Add Item"
 
-    it "displays a message" do
-      visit item_path(@item_1)
-      click_button "Add Item"
+    visit item_path(@item_1)
+    click_button "Add Item"
 
-      expect(page).to have_content("You now have 1 item of #{@item_1.name} in your cart.")
-      expect(current_path).to eq(items_path)
-    end
+    expect(page).to have_content("You now have 2 items of #{@item_1.name} in your cart.")
+  end
 
-    it "the message for multiple items" do
-      visit item_path(@item_1)
-      click_button "Add Item"
+  it "total of items in the cart" do
+    visit item_path(@item_1)
 
-      visit item_path(@item_2)
-      click_button "Add Item"
+    expect(page).to have_content("Cart: 0")
 
-      visit item_path(@item_1)
-      click_button "Add Item"
+    click_button "Add Item"
 
-      expect(page).to have_content("You now have 2 items of #{@item_1.name} in your cart.")
-    end
+    expect(page).to have_content("Cart: 1")
 
-    it "total of items in the cart" do
-      visit item_path(@item_1)
+    visit item_path(@item_2)
+    click_button "Add Item"
 
-      expect(page).to have_content("Cart: 0")
+    expect(page).to have_content("Cart: 2")
 
-      click_button "Add Item"
+    visit item_path(@item_1)
+    click_button "Add Item"
 
-      expect(page).to have_content("Cart: 1")
-
-      visit item_path(@item_2)
-      click_button "Add Item"
-
-      expect(page).to have_content("Cart: 2")
-
-      visit item_path(@item_1)
-      click_button "Add Item"
-
-      expect(page).to have_content("Cart: 3")
-    end
+    expect(page).to have_content("Cart: 3")
   end
 
   context "as a user" do
